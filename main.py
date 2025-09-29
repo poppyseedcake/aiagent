6d3c8eda9b5a5e2b14c822ee22097c3f3e7e382e
poppyseedcake <w.makowiec@gmail.com>
2025-09-29 16:51:59 +0200

    dsadas

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+    verbose = False
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
+
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, user_prompt, verbose)
+
+def generate_content(client, messages, user_prompt, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()

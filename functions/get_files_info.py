7c1145a2f377bdd062a53cf3bfbe7b4c78ee4fe5
poppyseedcake <w.makowiec@gmail.com>
2025-10-03 17:58:55 +0200

    dddsadsa

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,11 +1,17 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    if not os.path.isdir(directory):
-        raise Exception(f'Error: "{directory}" is not a directory')
+    try:
+        if not os.path.isdir(directory):
+            raise Exception(f'Error: "{directory}" is not a directory')
+    except Exception as e:
+        return(f"Error: {e}")
 
-    abs_path = os.path.abspath(directory)
-    full_path = os.path.join(working_directory, directory)
+    try:
+        abs_path = os.path.abspath(directory)
+        full_path = os.path.join(working_directory, directory)
+    except Exception as e:
+        return(f"Error: {e}")
 
     if not abs_path.startswitch(working_directory):
         raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')

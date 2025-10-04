7dbecb2b6a7c77425a91b12d528e3d73684757ae
poppyseedcake <w.makowiec@gmail.com>
2025-10-04 18:50:56 +0200

    test lorem

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+from config import *
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    abs_path_full_path = os.path.abspath(full_path)
+    abs_path_working = os.path.abspath(working_directory)
+    abs_work_with_sep = abs_path_working if abs_path_working.endswith(os.sep) else abs_path_working + os.sep
+    
+    if not (abs_path_full_path == abs_path_working or abs_path_full_path.startswith(abs_work_with_sep)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_path_full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if len(file_content_string) == MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def print_result(title, result):
     print(title)
@@ -7,12 +7,14 @@ def print_result(title, result):
     else:
         print(result)
 
-test1 = get_files_info("calculator", ".")
-test2 = get_files_info("calculator", "pkg")
-test3 = get_files_info("calculator", "/bin")
-test4 = get_files_info("calculator", "../")
+test1 = get_file_content("calculator", "lorem.txt") 
+#test2 = get_files_info("calculator", "pkg")
+#test3 = get_files_info("calculator", "/bin")
+#test4 = get_files_info("calculator", "../")
 
-print_result("Result for current directory:", test1)
-print_result("Result for 'pkg' directory:", test2)
-print_result("Result for '/bin' directory:", test3)
-print_result("Result for '../' directory:", test4)
+print(test1)
+
+#print_result("Result for current directory:", test1)
+#print_result("Result for 'pkg' directory:", test2)
+#print_result("Result for '/bin' directory:", test3)
+#print_result("Result for '../' directory:", test4)
